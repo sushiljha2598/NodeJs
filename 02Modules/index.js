a575b6021b36ba86dcf03824334e38f1600a682a
sushiljha2598 <78631531+sushiljha2598@users.noreply.github.com>
2024-07-03 13:33:09 +0530

    Create index.js

---
+++ 02Modules/index.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+// const {add, sub} = require("./math")
+
+// console.log(math.add(2,4));
+console.log(math.sub(6,2));
+// console.log(add(2,4));
+// console.log(sub(6,2));
