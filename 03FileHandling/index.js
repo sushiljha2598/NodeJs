3e6369b92a46bf2771970aa19b6f08b55f037769
sushiljha2598 <78631531+sushiljha2598@users.noreply.github.com>
2024-07-04 13:17:16 +0530

    Create index.js

---
+++ 03FileHandling/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// Sync
+// fs.writeFileSync("./test.txt","Hey there");
+
+// Async
+// fs.writeFile("./test.txt","Hey there Async",(err)=>{});
+
+// const result = fs.readFileSync("./contacts.txt","utf-8");
+// console.log(result);
+
+// fs.readFile("./contacts.txt","utf-8",(err,result)=>{
+//     if(err) console.log("Error:",err);
+//     else console.log(result);
+// })
+
+fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString());
+
+// fs.cpSync("./test.txt","./copy.txt");
+
+// fs.unlinkSync("./copy.txt");
+console.log(fs.statSync("./test.txt"));
+fs.mkdirSync("my-docs/a/b",{recursive: true});
